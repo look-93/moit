6c6c428fd95d9bea82d0184a28c8fcd0496838d9
look-93 <qhfk0961@gmail.com>
2026-06-18 18:19:45 +0900

    중간저장

--- src/main/java/com/moit/controller/UserMeetupController.java
+++ src/main/java/com/moit/controller/UserMeetupController.java
@@ -1,6 +1,7 @@
 package com.moit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserMeetupController {
 	
 	@RequestMapping("/list.do")
-	public String serchByUser() {
-		
+	public String serchByUser(Model model) {
+		model.addAttribute("menu", "meetup");
 		return "meetup/user/list";
 	}
 }
--- src/main/webapp/view/meetup/user/list.jsp
+++ src/main/webapp/view/meetup/user/list.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 	
-<%@ include file="../../inc/userHeader.jsp"%>
+<%-- <%@ include file="../../inc/userHeader.jsp"%> --%>
 <style>
 
 :root{
@@ -549,4 +549,4 @@ nav{
     </div>
 
 </div>
-<%@ include file="../../inc/userFooter.jsp"%>
+<%-- <%@ include file="../../inc/userFooter.jsp"%> --%>
